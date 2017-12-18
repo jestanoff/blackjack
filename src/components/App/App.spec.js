47388ed514c4aed3ade39fb95ccee9a47f6a983a
Stefan Dzhestanov <stefan@dzhestanov.email>
2017-12-18 11:02:21 +0000

    Adds Notes in the tests that are skiped

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import App from './App'
 import renderer from 'react-test-renderer'
 
+// NOTE: I cannot use enzyme with React 1.6 and Create React App as I need to setup tests and add an adaptor, but Jest doesn't accept my configurating in package.json. Also doing it with snapshots doesn't work as of random cards every time
 describe.skip('<App />', () => {
   it('should render proper markup', (): void => {
     const tree = renderer.create(<App />).toJSON()
--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Game from './Game'
 import { shallow } from 'enzyme'
 
+// NOTE: I cannot use enzyme with React 1.6 and Create React App as I need to setup tests and add an adaptor, but Jest doesn't accept my configurating in package.json. Also doing it with snapshots doesn't work as of random cards every time
 describe.skip('<Game />', () => {
   it('should render proper markup', () => {
     const wrapper = shallow(<Game />)
